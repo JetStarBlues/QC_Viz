93ed927ba77d704abb3fe85056ba423a12d617fb
Ryan Prior <ryanprior@hey.com>
2021-07-22 18:26:08 -0500

    Adds implementation of QCircuit-tagged strings & circuitToString

--- Circuit.js
+++ Circuit.js
@@ -19,3 +19,34 @@
       ]
 
 */
+
+const pegjs = require("pegjs")
+const fs = require("fs")
+const util = require("util")
+
+let data = ''
+try {
+  data = fs.readFileSync("./parser/qcircuit.pegjs", "utf8")
+} catch (err) {
+  console.error(err)
+}
+const parser = pegjs.generate(data);
+
+function QCircuit([circuitString]) {
+  return parser.parse(circuitString)
+}
+
+function circuitToString(qubits) {
+  const renderPart = part => {
+    switch(part.type) {
+    case "wire": return "-".repeat(part.width)
+    case "gate": return part.name
+    case "cnot": return {target: "*", control: "O", passthrough: "|"}[part.role]
+    case "measure": return "M>"
+    }
+    throw "Part not implemented."
+  }
+  return qubits.map(qubit =>
+    `|${qubit.basis}>${qubit.parts.map(part => renderPart(part)).join("")}`
+  ).join("\n")
+}
